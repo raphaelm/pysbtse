b38c1408032e9678bccc92892eaa8d2aeee1ffe4
Raphael Michel <michel@rami.io>
2024-12-28 12:52:10 +0100

    Always update time after selftest

--- sbtse/api.py
+++ sbtse/api.py
@@ -104,6 +104,7 @@ async def _worm_context(client_id, require_self_test=True) -> BaseWormContext:
                 logger.info(f"Running self test with client ID {client_id}...")
                 worm_context.run_self_test(client_id)
                 logger.info("Self test completed.")
+                _update_time(info)
             else:
                 logger.info(f"Running self test with client ID FAKE...")
                 try:
@@ -111,6 +112,7 @@ async def _worm_context(client_id, require_self_test=True) -> BaseWormContext:
                 except errors.WormErrorClientNotRegistered:
                     pass
                 logger.info("Self test completed.")
+                _update_time(info)
 
         if time.time() - last_time_update > info["maxTimeSynchronizationDelay"] * 0.8:
             _update_time(info)
